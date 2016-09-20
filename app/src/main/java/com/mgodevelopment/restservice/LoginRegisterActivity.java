fbe05701ba4ff2d70e809273cabd180b3e14f002
Martin Ouellet <moa21@sfu.ca>
2016-09-19 21:02:54 -0700

    241

--- app/src/main/java/com/mgodevelopment/restservice/LoginRegisterActivity.java
+++ app/src/main/java/com/mgodevelopment/restservice/LoginRegisterActivity.java
@@ -1,13 +1,125 @@
 package com.mgodevelopment.restservice;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.R.string.cancel;
 
 public class LoginRegisterActivity extends AppCompatActivity {
 
+    private UserLoginRegisterTask mUserLoginRegisterTask = null;
+    private EditText mEmailView;
+    private EditText mPasswordView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_register);
+        initViews();
+
     }
+
+    private void initViews() {
+
+        mEmailView = (EditText) findViewById(R.id.email);
+        mPasswordView = (EditText) findViewById(R.id.password);
+
+    }
+
+    public void attemptLoginRegister(View view) {
+
+        if (mUserLoginRegisterTask != null) {
+            return;
+        }
+
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+
+            mPasswordView.setError(getString(R.string.error_password_length));
+            focusView = mPasswordView;
+            cancel = true;
+
+        }
+
+        if (TextUtils.isEmpty(email)) {
+
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+
+        } else if (!isEmailValid(email)) {
+
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+
+        }
+
+        if (cancel) {
+            focusView.requestFocus();
+        } else {
+
+            mUserLoginRegisterTask = new UserLoginRegisterTask(email, password, view.getId() == R.id.email_sign_in_button);
+            mUserLoginRegisterTask.execute((Void) null);
+
+        }
+
+    }
+
+    private boolean isPasswordValid(String password) {
+        return password.length() > 4;
+    }
+
+    private boolean isEmailValid(String email) {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private void showProgress(final boolean isShow) {
+
+        findViewById(R.id.login_progress).setVisibility(isShow ? View.VISIBLE : View.GONE);
+        findViewById(R.id.login_form).setVisibility(isShow ? View.GONE : View.VISIBLE);
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
